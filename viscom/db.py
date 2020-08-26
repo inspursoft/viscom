4b500c4136db5d3cdde33b021a95be5cd748ee8a
wangkun_lc <wangkun_lc@inspur.com>
2020-08-26 15:21:07 +0800

    Update query for get capture item list.

--- viscom/db.py
+++ viscom/db.py
@@ -44,7 +44,7 @@ def delete_capture_item(app, group_name):
 
 def get_capture_item_list():
     return get_db().execute('''
-      select group_name, min(creation_time), min(update_time)
+      select group_name, min(source_name), min(creation_time), min(update_time)
         from capture_item group by group_name''').fetchall()
 
 def get_capture_item_list_by_group(group_name):
